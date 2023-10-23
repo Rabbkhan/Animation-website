6d2dbfb356426ab0092cbdd7286bb83fbf78aee2
Rabbil khan <rabbilkhan125@gmail.com>
2023-10-23 13:41:42 +0530

    setup code

---
+++ app.js
@@ -0,0 +1,10 @@
+var videcon = document.querySelector('#video-container');
+
+var playbtn = document.querySelector('#play');
+
+videcon.addEventListener('mouseenter', function(){
+    playbtn.style.opacity =1;
+    playbtn.style.scale =1;
+}  )
+
+
